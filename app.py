d3515aec032f8dd0b5459a9b2e2a88768d0ac1e0
Amila Poorna <arunodamkap@gmail.com>
2025-03-20 11:26:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ lab_dip_encoder = joblib.load('lab_dip_encoder.pkl')
 machine_code_encoder = joblib.load('machine_code_encoder.pkl')
 nylon_type_encoder = joblib.load("nylon_type_encoder.pkl")
 scaler = joblib.load('scaler.pkl')
-X_train = joblib.load("X_train.pkl")
+X_train_columns = joblib.load("X_train.pkl")
 
 st.title('Nylon Dyeing Recipe Status Predictor')
 
@@ -84,7 +84,7 @@ if st.button('Predict Status'):
     rft_data['Colour'] = colour_encoder.transform(rft_data['Colour'])
     rft_data['MachineCode'] = machine_code_encoder.transform(rft_data['MachineCode'])
 
-    rft_data = rft_data[X_train]
+    rft_data = rft_data[X_train_columns]
     
     prediction_class = classification_model.predict(input_data)
     st.session_state.prediction_class = prediction_class[0]
